bfa20e471e63378a3a281024ed2d29358c8e9508
New-Technologies123 <lka-ufa@yandex.ru>
2025-11-06 15:18:41 +0500

    delete changeLanguage

--- astro-i18next.config.ts
+++ astro-i18next.config.ts
@@ -2,7 +2,6 @@ import type { AstroI18nextConfig } from 'astro-i18next';
 
 const config: AstroI18nextConfig = {
   defaultLocale: 'ru',
-  locales: ['en', 'ru'],
   load: ['server', 'client'],
   i18nextServerPlugins: {
     '{initReactI18next}': 'react-i18next',
