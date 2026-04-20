873bb3de85a1f7c4e739ee420cb5db4f55b7e10e
New-Technologies123 <lka-ufa@yandex.ru>
2026-04-20 09:14:08 +0500

    new Careers and excel

--- src/components/content/careers/Careers.tsx
+++ src/components/content/careers/Careers.tsx
@@ -10,16 +10,16 @@ type TCareers = 'vacancy1' | 'vacancy2' | 'vacancy3' | 'vacancy4' | 'vacancy5' |
 export const Careers = () => {
 
     const vacancyLinks: Record<TCareers, string> = {
-        vacancy1: "https://ufa.hh.ru/vacancy/130706340?hhtmFromLabel=employer_vacancy_tab&hhtmFrom=employer",
-        vacancy2: "https://ufa.hh.ru/vacancy/130527326?hhtmFromLabel=employer_vacancy_tab&hhtmFrom=employer",
-        vacancy3: "https://ufa.hh.ru/vacancy/131034876?hhtmFromLabel=employer_vacancy_tab&hhtmFrom=employer",
-        vacancy4: "https://ufa.hh.ru/vacancy/130552730?hhtmFromLabel=employer_vacancy_tab&hhtmFrom=employer",        
-        vacancy5: "https://ufa.hh.ru/vacancy/130602170?hhtmFromLabel=employer_vacancy_tab&hhtmFrom=employer",        
-        vacancy6: "https://ufa.hh.ru/vacancy/130775738?hhtmFromLabel=employer_vacancy_tab&hhtmFrom=employer",
-        vacancy7: "https://ufa.hh.ru/vacancy/131234015?hhtmFromLabel=employer_vacancy_tab&hhtmFrom=employer",
-        vacancy8: "https://ufa.hh.ru/vacancy/130461562?hhtmFromLabel=employer_vacancy_tab&hhtmFrom=employer",
-        vacancy9: "https://ufa.hh.ru/vacancy/130996614?hhtmFromLabel=employer_vacancy_tab&hhtmFrom=employer",
-        vacancy10: "https://ufa.hh.ru/vacancy/130552730?hhtmFromLabel=employer_vacancy_tab&hhtmFrom=employer",
+        vacancy1: "https://ufa.hh.ru/vacancy/132221496?hhtmFromLabel=employer_vacancy_tab&hhtmFrom=employer",
+        vacancy2: "https://ufa.hh.ru/vacancy/131908550?hhtmFromLabel=employer_vacancy_tab&hhtmFrom=employer",
+        vacancy3: "https://ufa.hh.ru/vacancy/132058432?hhtmFromLabel=employer_vacancy_tab&hhtmFrom=employer",
+        vacancy4: "https://ufa.hh.ru/vacancy/131979965?hhtmFromLabel=employer_vacancy_tab&hhtmFrom=employer",        
+        vacancy5: "https://ufa.hh.ru/vacancy/132221506?hhtmFromLabel=employer_vacancy_tab&hhtmFrom=employer",        
+        vacancy6: "",
+        vacancy7: "",
+        vacancy8: "",
+        vacancy9: "",
+        vacancy10: "",
         vacancy11: "",
         vacancy12: "",
     };
@@ -58,80 +58,52 @@ export const Careers = () => {
                             <p>Заки Валиди 32/2</p>
                             <div className={Styles.team}>
                                 <Vacancy
-                                    header="Метролог"
-                                    title="Опыт работы: более 6 лет"
-                                    experience="Полная занятость"
-                                    employmentType="График: 5/2"
-                                    onClick={() => window.open(vacancyLinks.vacancy2, "_blank")}
-                                />
-                                <Vacancy
-                                    header="Ведущий экономист"
-                                    title="Опыт работы: 3–6 лет"
-                                    experience="Полная занятость"
-                                    employmentType="График: 5/2"
-                                    onClick={() => window.open(vacancyLinks.vacancy9, "_blank")}
-                                />                                
-                                <Vacancy
-                                    header="Инженер-электронщик"
-                                    title="Опыт работы: 1–3 года"
-                                    experience="Полная занятость"
-                                    employmentType="График: 5/2"
-                                    onClick={() => window.open(vacancyLinks.vacancy8, "_blank")}
-                                />
-                                <Vacancy
-                                    header="Тендерный специалист"
+                                    header="Инженер ПТО"
                                     title="Опыт работы: 1–3 года"
                                     experience="Полная занятость"
                                     employmentType="График: 5/2"
                                     onClick={() => window.open(vacancyLinks.vacancy1, "_blank")}
-                                />
-                                <Vacancy
-                                    header="Заместитель главного бухгалтера"
-                                    title="Опыт работы: 3–6 лет"
-                                    experience="Полная занятость"
-                                    employmentType="График: 5/2"
-                                    onClick={() => window.open(vacancyLinks.vacancy7, "_blank")}
-                                />
+                                />                               
                             </div>
                         </div>
                         <div className={Styles.vacancies}>
                             <p>Благоварская 16/2</p>
                             <div className={Styles.team}>
                                 <Vacancy
-                                    header="Электрогазосварщик ТТ"
-                                    title="Опыт работы: 1-3 года"
+                                    header="Руководитель проектов"
+                                    title="Опыт работы: 3–6 лет"
                                     experience="Полная занятость"
                                     employmentType="График: 5/2"
-                                    onClick={() => window.open(vacancyLinks.vacancy5, "_blank")}
+                                    onClick={() => window.open(vacancyLinks.vacancy2, "_blank")}
                                 />
                                 <Vacancy
-                                    header="Электрик"
-                                    title="Опыт работы: 1-3 года"
-                                    experience="Полная занятость"
-                                    employmentType="График: 5/2"
-                                    onClick={() => window.open(vacancyLinks.vacancy10, "_blank")}
-                                />
-                                <Vacancy
-                                    header="Слесарь-механик"
+                                    header="Специалист по охране труда"
                                     title="Опыт работы: 1-3 года"
                                     experience="Полная занятость"
                                     employmentType="График: 5/2"
                                     onClick={() => window.open(vacancyLinks.vacancy3, "_blank")}
                                 />
                                 <Vacancy
-                                    header="Оператор ЧПУ фрезерного станка"
+                                    header="Юрист"
                                     title="Опыт работы: 1–3 года"
                                     experience="Полная занятость"
-                                    employmentType="График: 2/2"
-                                    onClick={() => window.open(vacancyLinks.vacancy6, "_blank")}
-                                />                                
+                                    employmentType="График: 5/2"
+                                    onClick={() => window.open(vacancyLinks.vacancy4, "_blank")}
+                                />
                             </div>
                         </div>
-                        {/* <div className={Styles.vacancies}>
+                        <div className={Styles.vacancies}>
                             <p>Вахта</p>
                             <div className={Styles.team}>
+                                <Vacancy
+                                    header="Водитель (категория С) ХМАО"
+                                    title="Опыт работы: не требуется"
+                                    experience="Вахта на 30, 45 или 60 смен"
+                                    employmentType=""
+                                    onClick={() => window.open(vacancyLinks.vacancy5, "_blank")}
+                                />
                             </div>
-                        </div> */}
+                        </div>
                     </div>
                 </> 
             )}
